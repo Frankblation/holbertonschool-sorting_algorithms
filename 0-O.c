d12bae8e62bb4aab03e3760fee9f4833f01d71f1
Frankblation <frankblation@gmail.com>
2023-08-14 20:02:52 -0700

    1st push on sorting

---
+++ 0-O.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Best Case: O(n)\n");
+    printf("Average Case: O(n^2)\n");
+    printf("Worst Case: O(n^2)\n");
+
+    return 0;
+}
---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * swap - Swaps two integers.
+ * @x: Pointer to the first integer.
+ * @y: Pointer to the second integer.
+ */
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+/**
+ * bubble_sort - Sorts an array of integers using Bubble sort algorithm.
+ * @array: Pointer to the array.
+ * @size: Size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap(&array[j], &array[j + 1]);
+            }
+        }
+    }
+}
