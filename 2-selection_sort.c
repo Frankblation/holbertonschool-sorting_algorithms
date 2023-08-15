b644e9d95bddaa438b2f0e7cbd93fe25c429c32e
Frankblation <frankblation@gmail.com>
2023-08-14 21:26:30 -0700

    2nd for 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,26 +1,31 @@
 #include "sort.h"
+/**
+* selection_sort - sorts array of integers into ascending order*
+* @array: array of integers
+* @size: size of array
+*/
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        size_t min_index = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t min_index = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            int temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
-        }
-    }
+		if (min_index != i)
+		{
+			int temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+		}
+	}
 }
